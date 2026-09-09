65b547a816a71285b54d7c45697235b9a53a581e
Chi Ma <adelaideneocs@gmail.com>
2026-09-09 12:00:36 +1000

    turbo error

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  turbopack: {},
+
   webpack(config, { webpack }) {
     config.plugins.push(
       new webpack.NormalModuleReplacementPlugin(/^node:/, (resource: { request: string }) => {
